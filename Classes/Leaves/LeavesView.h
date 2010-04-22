8bf0d2ab8eff7d8df8f0727b0157cce0a5d19d96
Tom Brow <tebrow@gmail.com>
2010-04-21 23:43:09 -0700

    Add Leaves.

---
+++ Classes/Leaves/LeavesView.h
@@ -0,0 +1,63 @@
+//
+//  LeavesView.h
+//  Leaves
+//
+//  Created by Tom Brow on 4/18/10.
+//  Copyright 2010 Tom Brow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@protocol LeavesViewDataSource;
+@protocol LeavesViewDelegate;
+
+@interface LeavesView : UIView {
+	CALayer *topPage;
+	CALayer *topPageOverlay;
+	CAGradientLayer *topPageShadow;
+	
+	CALayer *topPageReverse;
+	CALayer *topPageReverseImage;
+	CALayer *topPageReverseOverlay;
+	CAGradientLayer *topPageReverseShading;
+	
+	CALayer *bottomPage;
+	CAGradientLayer *bottomPageShadow;
+	
+	CGFloat leafEdge;
+	NSUInteger currentPageIndex;
+	NSUInteger numberOfPages;
+	id<LeavesViewDataSource> dataSource;
+	id<LeavesViewDelegate> delegate;
+	
+	CGSize pageSize;
+	CGPoint touchBeganPoint;
+	BOOL touchIsActive;
+	CGRect nextPageRect, prevPageRect;
+	BOOL interactionLocked;
+}
+
+@property (assign) id<LeavesViewDataSource> dataSource;
+@property (assign) id<LeavesViewDelegate> delegate;
+
+- (void) reloadData;
+
+@end
+
+
+@protocol LeavesViewDataSource <NSObject>
+
+- (NSUInteger) numberOfPagesInLeavesView:(LeavesView*)leavesView;
+- (void) renderPageAtIndex:(NSUInteger)index inContext:(CGContextRef)ctx;
+
+@end
+
+@protocol LeavesViewDelegate <NSObject>
+
+@optional
+
+- (void) leavesView:(LeavesView *)leavesView didTurnToPageAtIndex:(NSUInteger)pageIndex;
+
+@end
+
